241b03a56cc97671128e75ead2c0d46fa41c7b09
saipabi <saipabi123@gmail.com>
2025-03-14 04:53:59 +0530

    kljkljkljkljklj

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",  // <-- Add this line to fix Netlify routing issues
+  base: "/", // <-- This ensures correct routing on Netlify
 });
